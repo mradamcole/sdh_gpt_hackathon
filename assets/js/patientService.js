4d9630cf5aaf8fc8a12345ab8f53ce1374e78cf4
Alan Fernandes <alanfernandes@Alans-MacBook-Pro.local>
2023-09-15 17:14:35 -0400

    updated headers

--- assets/js/patientService.js
+++ assets/js/patientService.js
@@ -103,7 +103,6 @@ function submitData() {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*',
             "Access-Control-Allow-Credentials":"true",
-            "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
             "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
         },
         body: JSON.stringify(patientData)
